e66b5146647a94eb76d8627a34cd5111afc41065
tm <tm.oxenfree@gmail.com>
2025-09-20 11:06:19 +0700

    layout ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,10 @@
 export const load = () => {
-	const links = [
-		{ title: 'Home', href: '/' },
-		{ title: 'About', href: '/about' }
-	];
 	return {
 		nav: {
-			links
+			links: [
+				{ title: 'Home', href: '/' },
+				{ title: 'About', href: '/about' }
+			]
 		}
 	};
 };
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,22 +1,20 @@
 export const load = () => {
-  const heroActions = [
-          { title: 'Services', href: '/' },
-          { title: 'Signup', href: '#signup' }
-      ];
-
-  return {
-    meta: {
-      // title: 'Home',
-      // desc: 'Home page.'
-    },
-    hero: {
-      title: 'HERO',
-      dec: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero ab atque doloremque, harum sed cupiditate unde dignissimos error tempore veniam doloribus ducimus, quas cumque omnis necessitatibus sapiente corrupti exercitationem placeat?',
-      heroActions
-    },
-    signup: {
-      title: 'Signup',
-      id: 'signup'
-    }
-  };
+	return {
+		meta: {
+			// title: 'Home',
+			// desc: 'Home page.'
+		},
+		hero: {
+			title: 'HERO',
+			dec: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero ab atque doloremque, harum sed cupiditate unde dignissimos error tempore veniam doloribus ducimus, quas cumque omnis necessitatibus sapiente corrupti exercitationem placeat?',
+			heroActions: [
+				{ title: 'Services', href: '/' },
+				{ title: 'Signup', href: '#signup' }
+			]
+		},
+		signup: {
+			title: 'Signup',
+			id: 'signup'
+		}
+	};
 };
